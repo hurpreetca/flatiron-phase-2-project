94163e6f112ad5d431b3fe75aba6a45133dc7084
Harpreet Singh <hurpreet@icloud.com>
2023-07-06 17:36:13 -0400

    Home component is working properly with desired content

--- src/Components/GhibliMovieDetail/GhibliMovieDetails.js
+++ src/Components/GhibliMovieDetail/GhibliMovieDetails.js
@@ -14,84 +14,86 @@ function GhibliMovieDetails() {
       .then((movie) => setSelectedMovie(movie));
   }, [id]);
   return (
-    <Grid>
-      <Grid.Column width={1}></Grid.Column>
-      <Grid.Column width={4}>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Image src={selectedMovie.movie_banner} size="massive" circular />
-      </Grid.Column>
-      <Grid.Column width={6}>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Card className="movie-detail-card" centered raised>
-          <Card className="movie-title-card" color="violet">
-            <Card.Content>
-              <br />
-            </Card.Content>
-            <Card.Header textAlign="center">
-              <Icon disabled name="video camera" size="big" color="blue" />
-              <b className="movie-detail-name">{selectedMovie.title}</b>
-              <br />
-              {selectedMovie.original_title}
-              <br />
-              <br />
-              <br />
-            </Card.Header>
+    <div className="movie-detail-div">
+      <Grid>
+        <Grid.Column width={1}></Grid.Column>
+        <Grid.Column width={4}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Image src={selectedMovie.movie_banner} size="massive" circular />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <Card className="movie-detail-card" centered raised>
+            <Card className="movie-title-card" color="violet">
+              <Card.Content>
+                <br />
+              </Card.Content>
+              <Card.Header textAlign="center">
+                <Icon disabled name="video camera" size="big" color="blue" />
+                <b className="movie-detail-name">{selectedMovie.title}</b>
+                <br />
+                {selectedMovie.original_title}
+                <br />
+                <br />
+                <br />
+              </Card.Header>
+            </Card>
+            <Card color="purple">
+              <Card.Header textAlign="center">
+                <br />
+                <b>Directed By: {selectedMovie.director}</b>
+                <br />
+                <b>Produced By: {selectedMovie.producer}</b>
+                <br />
+                <br />
+              </Card.Header>
+            </Card>
+            <Card color="blue">
+              <Card.Content textAlign="center">
+                <b>Description: </b>
+                <br />
+                {selectedMovie.description}
+              </Card.Content>
+            </Card>
+            <Card color="teal">
+              <Card.Header textAlign="center">
+                <br />
+                <b>Release Year: {selectedMovie.release_year}</b>
+                <br />
+                <b>Runtime: {selectedMovie.running_time} mins</b>
+                <br />
+                <b>Rating: {selectedMovie.rt_score}%</b>
+                <br />
+                <br />
+              </Card.Header>
+            </Card>
           </Card>
-          <Card color="purple">
-            <Card.Header textAlign="center">
-              <br />
-              <b>Directed By: {selectedMovie.director}</b>
-              <br />
-              <b>Produced By: {selectedMovie.producer}</b>
-              <br />
-              <br />
-            </Card.Header>
-          </Card>
-          <Card color="blue">
-            <Card.Content textAlign="center">
-              <b>Description: </b>
-              <br />
-              {selectedMovie.description}
-            </Card.Content>
-          </Card>
-          <Card color="teal">
-            <Card.Header textAlign="center">
-              <br />
-              <b>Release Year: {selectedMovie.release_year}</b>
-              <br />
-              <b>Runtime: {selectedMovie.running_time} mins</b>
-              <br />
-              <b>Rating: {selectedMovie.rt_score}%</b>
-              <br />
-              <br />
-            </Card.Header>
-          </Card>
-        </Card>
-      </Grid.Column>
-      <Grid.Column width={4}>
-        <br />
+        </Grid.Column>
+        <Grid.Column width={4}>
+          <br />
 
-        <br />
-        <br />
-        <br />
-        <Image src={selectedMovie.image} size="large" circular />
-      </Grid.Column>
-    </Grid>
+          <br />
+          <br />
+          <br />
+          <Image src={selectedMovie.image} size="large" circular />
+        </Grid.Column>
+      </Grid>
+    </div>
   );
 }
 
